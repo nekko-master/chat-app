e2804d63e9740250e08697ef68eeec6d7aa13d61
nekko-master <kanomaster00@gmail.com>
2025-09-05 21:14:29 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,19 @@ class RoomsController < ApplicationController # Rooms コントローラー。Ap
   def new # new アクション（新規作成フォームを表示するときに呼ばれる）
     @room = Room.new  # Room モデルの空のインスタンスを作成し、@room に代入
   end                 # このインスタンスはビュー(new.html.erb)でフォーム生成に使用される
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
